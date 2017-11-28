1cc523452f7211f35f6efd71d976f8b31515f357
Unknown <rtomilobil@gmail.com>
2017-11-28 19:35:29 +0100

    init solution

---
+++ SaintSender/SaintSender/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using SaintSender;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GmailQuickstart
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Connection connection = new Connection();
+            var service = connection.GetService();
+            MailService mailService = new MailService(service);
+            var result = mailService.AllMessage();
+            result[0].Serialize();
+            /*foreach (var item in result)
+            {
+                Console.WriteLine(item.Text);
+            }*/
+
+           
+            //Console.ReadKey();
+            
+
+        }
+    }
+}
