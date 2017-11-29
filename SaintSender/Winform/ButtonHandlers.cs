061c05cbddd39c2215c83cb65448e64b1b95e4ef
Unknown <rtomilobil@gmail.com>
2017-11-29 19:00:53 +0100

    add handlers for buttonevents

---
+++ SaintSender/Winform/ButtonHandlers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform
+{
+    public partial class Form1
+    {
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            newMailForm.Form1 form = new newMailForm.Form1();
+            form.Show();
+        }
+
+        private void inboxBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void draftBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trashBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void archiveBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
