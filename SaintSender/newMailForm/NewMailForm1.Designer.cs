ebe0b6eec3e1cdb00438b598b015f597b1dff67d
Unknown <rtomilobil@gmail.com>
2017-11-30 20:46:46 +0100

    new email form done

--- SaintSender/newMailForm/NewMailForm1.Designer.cs
+++ SaintSender/newMailForm/NewMailForm1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace newMailForm
 {
-    partial class Form1
+    partial class NewMailForm1
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.msgBox = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.sendBtn = new System.Windows.Forms.Button();
-            this.saveBtn = new System.Windows.Forms.Button();
-            this.exitBtn = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.toBox = new System.Windows.Forms.TextBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.subjBox = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -45,18 +47,31 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.msgBox);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(963, 337);
             this.panel1.TabIndex = 0;
             // 
+            // msgBox
+            // 
+            this.msgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.msgBox.Location = new System.Drawing.Point(4, 4);
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.Size = new System.Drawing.Size(956, 330);
+            this.msgBox.TabIndex = 0;
+            // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.subjBox);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.toBox);
             this.panel2.Controls.Add(this.exitBtn);
             this.panel2.Controls.Add(this.saveBtn);
             this.panel2.Controls.Add(this.sendBtn);
@@ -65,25 +80,21 @@
             this.panel2.Size = new System.Drawing.Size(963, 54);
             this.panel2.TabIndex = 1;
             // 
-            // sendBtn
+            // label1
             // 
-            this.sendBtn.Location = new System.Drawing.Point(452, 14);
-            this.sendBtn.Name = "sendBtn";
-            this.sendBtn.Size = new System.Drawing.Size(75, 23);
-            this.sendBtn.TabIndex = 0;
-            this.sendBtn.Text = "Send";
-            this.sendBtn.UseVisualStyleBackColor = true;
-            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(219, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "To:";
             // 
-            // saveBtn
+            // toBox
             // 
-            this.saveBtn.Location = new System.Drawing.Point(692, 13);
-            this.saveBtn.Name = "saveBtn";
-            this.saveBtn.Size = new System.Drawing.Size(75, 23);
-            this.saveBtn.TabIndex = 1;
-            this.saveBtn.Text = "Save";
-            this.saveBtn.UseVisualStyleBackColor = true;
-            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            this.toBox.Location = new System.Drawing.Point(266, 13);
+            this.toBox.Name = "toBox";
+            this.toBox.Size = new System.Drawing.Size(373, 22);
+            this.toBox.TabIndex = 3;
             // 
             // exitBtn
             // 
@@ -95,34 +106,43 @@
             this.exitBtn.UseVisualStyleBackColor = true;
             this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
             // 
-            // textBox1
+            // saveBtn
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(4, 4);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(956, 330);
-            this.textBox1.TabIndex = 0;
+            this.saveBtn.Location = new System.Drawing.Point(755, 13);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             // 
-            // textBox2
+            // sendBtn
             // 
-            this.textBox2.Location = new System.Drawing.Point(44, 14);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(373, 22);
-            this.textBox2.TabIndex = 3;
+            this.sendBtn.Location = new System.Drawing.Point(664, 12);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 0;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "To:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Subject:";
             // 
-            // Form1
+            // subjBox
+            // 
+            this.subjBox.Location = new System.Drawing.Point(71, 13);
+            this.subjBox.Name = "subjBox";
+            this.subjBox.Size = new System.Drawing.Size(142, 22);
+            this.subjBox.TabIndex = 5;
+            // 
+            // NewMailForm1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -130,7 +150,7 @@
             this.ControlBox = false;
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.Name = "NewMailForm1";
             this.Text = "New E-mail";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -143,13 +163,15 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox msgBox;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button exitBtn;
         private System.Windows.Forms.Button saveBtn;
         private System.Windows.Forms.Button sendBtn;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox toBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox subjBox;
     }
 }
 
---
+++ SaintSender/newMailForm/NewMailForm1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using SaintSender;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace newMailForm
+{
+    public partial class NewMailForm1 : Form
+    {
+        Validator validator;
+
+        private string text, to, subject;
+        public string To { get => to; set => to = value; }
+        public string Text1 { get => text; set => text = value; }
+        public string Subject { get => subject; set => subject = value; }
+
+        private bool isSave, isSend, isValid;
+        public bool IsSave { get => isSave; set => isSave = value; }
+        public bool IsSend { get => isSend; set => isSend = value; }
+        public bool IsValid { get => isValid; set => isValid = value; }
+        
+        public NewMailForm1()
+        {
+            InitializeComponent();
+            validator = new Validator();
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            IsSave = false;
+            IsSend = true;
+            Memorizer(isSend);
+            // TODO
+            // validate and verify, save into variables, close
+            Exit();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            IsSave = true;
+            IsSend = false;
+            Memorizer(isSend);
+            // TODO
+            // SAVE into drafts
+            Exit();
+        }
+        private void Memorizer(bool isSend)
+        {
+            Text1 = msgBox.Text;
+            Subject = subjBox.Text;
+            if (isSend)
+            {
+                if (validator.MailValidator(toBox.Text))
+                {
+                    To = toBox.Text;
+                    toBox.BackColor = Color.White;
+                    isValid = true;
+                } else
+                {
+                    toBox.BackColor = Color.Red;
+                    isValid = false;
+                }
+            }
+            if (IsSave) { isValid = true; }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(msgBox.Text))
+            {
+                var answer = MessageBox.Show("Warning", "Do you want to save it into the Draft folder?", MessageBoxButtons.YesNo);
+                if (answer == DialogResult.Yes)
+                {
+                    IsSave = true;
+                    IsSend = false;
+                    Memorizer(isSend);
+                }
+            } else
+            {
+                isValid = true;
+            }
+            Exit();
+        }
+        private void Exit()
+        {
+            if (IsValid)
+            {
+                Close();
+            }
+        }
+    }
+}
